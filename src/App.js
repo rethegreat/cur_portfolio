78905b6b55ce785e04407d21e4535ec956ed2bb7
Boaz <boazcheung2003@gmail.com>
2022-04-23 15:38:11 -0400

    deleted react logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Student Web Developer</h1>
+      <h1>Boaz Cheung</h1>
     </div>
   );
 }
