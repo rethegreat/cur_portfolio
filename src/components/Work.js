8630b049ccb3e573c4a7e2bc8267784111d3e161
Boaz <boazcheung2003@gmail.com>
2022-04-23 18:38:48 -0400

    beginning of second part

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,13 +1,14 @@
+import './Work.css'
 import React from 'react'
 
 export default function Work(){
     return (
-        <div>
-            <h3>
+        <div className='aboutMe'>
+            <h3 className='work'>
                 Passionate about building a future with web and applications that are accessible and inclusive for all. Positively impacting the life for those that use these technologies.
             </h3>
 
-            <h3>
+            <h3 className='self'>
                 In my free times I boulder, and solve different type of problems to challenge myself think critically about each situation. 
             </h3>
         </div>
