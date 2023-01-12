740b8bcbef3111ba829cd7fa7259a58d40170774
Wo Ming Boaz Cheung <92407344+rethegreat@users.noreply.github.com>
2023-01-11 23:08:03 -0500

    updated with new projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Projects.css'
 import Umate from './umate.jpg'
-import medival from './medival.jpg'
+import spending from './spending.jpeg'
 import amitee from './amitee.jpg'
 import Aos from "aos"
 import 'aos/dist/aos.css'
@@ -19,7 +19,7 @@ export default function Project(props){
             <h2 className='projectTitle' data-aos="zoom-in">My Projects</h2>
             <div className={props.windowWidth > 900 ? 'project' : 'mobileProject'}>
                 <div className='projectText' data-aos="fade-right">
-                    <h1>Amitee</h1>
+                    <a href='https://github.com/PotatoAuthor/HackerSnackers' className='projectTitle'>Amitee</a>
                     <p>
                         A desktop app that searches the user's Google Calendar schedule info to suggest recommendations 
                         for how they can spend their free time with friends
@@ -29,9 +29,9 @@ export default function Project(props){
             </div>
             <div className={props.windowWidth > 900 ? 'project uMate' : 'mobileProject'}>
                 <div className='projectText ' data-aos="fade-left">
-                    <h1>UMate</h1>
+                    <a href='https://github.com/CSC207-2022F-UofT/course-project-blob-daily' className='projectTitle'>Blob Daily</a>
                     <p>
-                        An app created for university students to find roomates for off campus housing using match making algorithms
+                        An app created for to incentivise productivity by introducing a gamification of task
                     </p>
                 </div>
                 <img src={Umate} alt='a group of friends' className='projectPicture'data-aos="fade-right"/>
@@ -39,12 +39,12 @@ export default function Project(props){
             </div>
             <div className={props.windowWidth > 900 ? 'project' : 'mobileProject'}>
                 <div className='projectText' data-aos="fade-right">
-                    <h1>Medival Life</h1>
+                    <a href='https://github.com/rethegreat/trackie' className='projectTitle'>Trackie</a>
                     <p>
-                        A game enabling you to experience the life of a medival peasent through everyday actions
+                        Web app is designed to help you keep track of your spending habits and make informed decisions about your finances.
                     </p>
                 </div>
-                <img src={medival} alt='a group of friends' className='projectPicture' data-aos="fade-left"/>
+                <img src={spending} alt='a group of friends' className='projectPicture' data-aos="fade-left"/>
             </div>
         </div>
     )
